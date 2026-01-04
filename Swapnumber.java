e21f9de05fcb6a46439908ffaa088f4b77b4bc1c
Harini204 <144297216+Harini204@users.noreply.github.com>
2026-01-04 19:59:45 +0530

    Add files via upload

---
+++ Swapnumber.java
@@ -0,0 +1,25 @@
+package basicJava;
+
+public class Swapnumber {
+	public static void main(String[] args) {
+	       int a = 10;
+	       int b = 20;
+	       int temp;
+	       
+	       
+	       System.out.println("Before Swap");
+	       System.out.println("a= "+a);
+	       System.out.println("b ="+b);
+	       
+	       temp= a;
+	       a = b;
+	       b = temp;
+	       
+	       
+	       System.out.println("After Swap");
+	       System.out.println("a = "+a);
+	       System.out.println("b = "+b);
+	       
+	}
+
+}
